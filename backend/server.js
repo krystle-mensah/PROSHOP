745475df782de4195d5185dd4d1bc25f76da252e
krystle Mensah <web-development@krystlemensah.co.uk>
2020-12-13 14:05:59 +0000

    adding colors for the terminal

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 // bring in express with require function in express. this common js synex
 import express from 'express'
 import dotenv  from 'dotenv'
+
+import colors from 'colors'
+
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -30,5 +33,5 @@ const PORT = process.env.PORT || 5000
 //then we take app and listen on port 5000 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
