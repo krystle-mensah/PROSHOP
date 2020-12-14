91caa6f75d622a92abcfb7f89eec015b13d925aa
krystle Mensah <web-development@krystlemensah.co.uk>
2020-12-14 14:24:14 +0000

    adding data

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import monogoose from 'mongoose'
+
+const userSchema = monogoose.Schema({
+  name: {
+    type: String, 
+    required: true 
+  },
+  email: {
+    type: String, 
+    required: true,
+    unique: true 
+  },
+  password: {
+    type: String, 
+    required: true 
+  },
+  isAdmin: {
+    type: Boolean, 
+    required: true,
+    default: false 
+  },
+}, {
+  timestamps: true
+})
+
+const User = monogoose.model('User', userSchema)
+
+export default User
